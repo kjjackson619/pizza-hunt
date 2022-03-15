271d6b967035afa0ab47e716b9888bae12d9bb43
KJ Jackson <kjjackson619@yahoo.com>
2022-03-14 21:00:06 -0400

    replies

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,4 +1,5 @@
 const { Comment, Pizza } = require('../models');
+const { rawListeners } = require('../models/Comment');
 
 const commentController = {
     addComment({ params, body }, res) {
@@ -18,6 +19,31 @@ const commentController = {
                 }
                 res.json(dbPizzaData);
             })
+            .catch(err => res.json(err));
+    },
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData)
+            })
+            .catch(err => res.json(err));
+    },
+
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+            .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => res.json(err));
     },
 
